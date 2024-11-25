60074cf49bdd62e939e89146cf7a4fe5c3903b1d
Sid0130 <choiho0130@gmail.com>
2024-11-25 15:47:41 +0900

    .

---
+++ lab03_control/src/com/itwill/switch03/Season.java
@@ -0,0 +1,10 @@
+package com.itwill.switch03;
+
+
+// enum : enumerated type(열거형 타입). 상수(constant)들을 정의하는 특별한 종류의 클래스.
+
+public enum Season { //이름은 대문자 관습.
+	SPRING, SUMMER, FALL, WINTER;
+	// 특별한 케이스 이것 외에 다른 값을 가질 수 없으면 타입을 만드는것
+
+}
